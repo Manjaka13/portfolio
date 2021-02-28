34cde5d9442caee88840637e95ff10ce2c2229da
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-02-28 18:59:00 +0100

    Added resume and download button

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,8 +1,11 @@
 import React from "react";
+import {FontAwesomeIcon as Icon} from "@fortawesome/react-fontawesome";
+import {faDownload} from "@fortawesome/free-solid-svg-icons";
 import Textblock from "../Textblock/Textblock";
 import {Title, Text} from "../vrc/";
 import "./AboutMe.scss";
 import avatar from "../assets/avatar_harijaona_rajaonson.jpg";
+import resume from "../assets/resume_harijaona_rajaonson_developer.docx";
 
 function AboutMe() {
 	const age = new Date().getFullYear() - 1996;
@@ -12,6 +15,9 @@ function AboutMe() {
 			<Title className="title2" h3 xlg>I build high quality apps !</Title>
 			<div className="separator"></div>
 			<Text>I'm a full stack developer based in Madagascar, specialized in Javascript. I am familiar with the <span className="bold">MERN</span> stack technologies. I have 2 years of Freelance experience and more than 6 years in general programming. I enjoy building pixel perfect apps for the web. I like them to be fast, beautiful, secured and with scalable code. Don't hesitate to talk with me, I speak English and French fluently apart of my native language.</Text>
+			<button className="vrc-button vrc-text resume" title="Get my resume !" onClick={() => {document.location = resume;}}>
+				<Icon icon={faDownload} /> Download my resume
+			</button>
 			<div className="info-card">
 				<div className="left">
 					<div className="box">
